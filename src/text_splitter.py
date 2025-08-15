c58a9a8887391a5f5e0c251ae7d46fb878a27081
Sheldon Bish <sbish33@gmail.com>
2025-08-14 21:44:33 -0400

    Added sample document in src/text samples and completed the text splitter/chunker.

--- src/text_splitter.py
+++ src/text_splitter.py
@@ -1,9 +1,26 @@
 from langchain_text_splitters import CharacterTextSplitter
 from neo4j_graphrag.experimental.components.text_splitters.langchain import LangChainTextSplitterAdapter
 import asyncio
+import pdb
+import ast
 
-async def text_splitter(input_text: str):
+async def text_splitter(input_text: str, chunk_size: int = 200, chunk_overlap: int = 100):
+    ## Splits Document into chunks of size <chunk_size> or smaller with <chunk_overlap>
+    #  character overlaps between consecutive chunks. Yields an object <splits> that contains
+    #  text, index (of chunk), metadata and uid for each chunk. A dictionary can be obtained 
+    #  by splits.chunks[10].model_dump().
     splitter = LangChainTextSplitterAdapter(
-        CharacterTextSplitter(chunk_size=4000, chunk_overlap=200, separator=".")
+        CharacterTextSplitter(
+            chunk_size=chunk_size, 
+            chunk_overlap=chunk_overlap, 
+            separator=".")
     )
-    await splitter.run(text="Hello World. Life is beautiful.")
+    
+    splits = await splitter.run(text=input_text)
+    return splits
+
+if __name__ == "__main__":
+    with open('text_samples/trump_putin.txt', 'r') as file_object:
+        sample = file_object.read().replace("\n","").replace("\'s","s")
+
+    splits = asyncio.run(text_splitter(sample))
